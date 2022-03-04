6d30f177c4391ed25efbb0a3996832abb9117662
Mukhammaddiyor Khaydaraliev <mukhammaddiyorkhaydaraliev@Mukhammaddiyorui-MacBookPro.local>
2022-03-04 21:48:19 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      console.log(response.data)
+      setAllData(response.data);
+      setFilteredData(response.data);
+    })
+    .catch(error => {
+      console.log('Error gettinng fake data: ' + error);
+    })
+  }, []);
+  const [allData,setAllData] = useState([]);
+  const [filteredData,setFilteredData] = useState(allData);
+  const handleSearch = (event) =>{
+    let value = event.target.value.toLowerCase();
+    let result = [];
+    console.log(value);
+    result = allData.filter((data) => {
+      return data.title.search(value) !=-1;
+  
+    });
+    setFilteredData(result);
+  }
+  const styles = {
+    display:'inline',
+    width: '100%',
+    height: 100,
+    float: 'left',
+    padding: 1,
+    //border: '0.5px solid black',
+    marginBottom: 10,
+    marginRight: 10
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Posts</h1>
+      <div style={{ margin: '0 auto', marginnTop: '10%'}}>
+        <label>Filter by entring a post title: </label>
+        <input type="text" placeholder="Enter Post Title Here" onChange={(event) => handleSearch(event)} />
+      </div>
+      <div style={{padding:20}}>
+        {filteredData.map((value, index)=>{
+          return(
+            <div>
+              <div style={styles} key={value.id}>
+                <h4>Post Title: {value.title}</h4>
+                {value.body}
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
